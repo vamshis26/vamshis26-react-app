7255e9966f55d9cec35f1f0cef56d48767043bc0
vamshis26clear <your.email@example.com>
2024-12-29 22:48:20 -0800

    dep

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-// import Login from './Login';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    {/* <Login /> */}
   </React.StrictMode>
 );
 
