72a362cf6cbb01a6bc9619c2294864b1c54e0eac
vamshis26clear <your.email@example.com>
2024-12-30 18:49:02 -0800

    render

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Login component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  
+  // Check if the Login component is rendered
+  const loginElement = screen.getByText(/login/i); 
+  expect(loginElement).toBeInTheDocument();
 });
